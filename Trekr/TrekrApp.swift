90e7c161f4d6dd2c010770303f8d4826d63c04ed
Russell Gordon <github@russellgordon.ca>
2020-12-22 20:02:17 -0500

    Improved app icon

--- Trekr/TrekrApp.swift
+++ Trekr/TrekrApp.swift
@@ -10,6 +10,8 @@ import SwiftUI
 @main
 struct TrekrApp: App {
     
+    // App icon photo by Maarten van den Heuvel on https://unsplash.com/s/photos/beach
+    
     // @StateObject makes the instance of the Locations class stay alive for use elsewhere
     // Requires that Locations class conform to ObservableObject protocol
     @StateObject var locations = Locations()
