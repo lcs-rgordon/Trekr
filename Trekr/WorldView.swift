405614271f862ea03a022b8ce5b90e1753e74ebf
Russell Gordon <rgordon@lcs.on.ca>
2021-01-23 05:32:05 -0500

    Reformat

--- Trekr/WorldView.swift
+++ Trekr/WorldView.swift
@@ -15,9 +15,9 @@ struct WorldView: View {
     
     // Centre on London, show most of Europe
     @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222,
-                                                                   longitude: -0.1275),
-                                    span: MKCoordinateSpan(latitudeDelta: 40,
-                                                           longitudeDelta: 40))
+                                                                          longitude: -0.1275),
+                                           span: MKCoordinateSpan(latitudeDelta: 40,
+                                                                  longitudeDelta: 40))
     
     var body: some View {
         
