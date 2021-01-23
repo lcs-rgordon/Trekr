aaf1a87552dc270632c09562b71c099d896a573a
Russell Gordon <rgordon@lcs.on.ca>
2021-01-23 06:45:41 -0500

    Added comments to Tips view.

--- Trekr/Views/TipsView.swift
+++ Trekr/Views/TipsView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct TipsView: View {
     
+    // List of tips to be shown
     let tips: [Tip]
     
     var body: some View {
+        
+        // Iterate over list of tips
+        // Show data stored in "children" property, if that exists for a given tip
         List(tips, id: \.text, children: \.children) { tip in
+            
+            // When there are children for a tip, show an icon, make text bold
             if tip.children != nil {
                 Label(tip.text, systemImage: "quote.bubble")
                     .font(.headline)
             } else  {
+                // ...otherwise, just show the tip
                 Text(tip.text)
             }
         }
